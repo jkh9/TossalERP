c001102b21aea40805034651a3c9c02abd757fa1
jkh9L <jkh9L@MOISEX-LAPTOP>
2023-03-16 19:29:23 +0100

    Agregar archivos de proyecto.

---
+++ Musico.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TossalERP
+{
+    internal class Musico
+    {
+        string Nombre;
+        Instrumento Instrumento;
+
+    }
+}
