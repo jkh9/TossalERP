f675c61c750e5550f7e7460b9cc81cb27b5302b4
Moisex <34688294+jkh9@users.noreply.github.com>
2023-03-16 20:32:37 +0100

    Minor Changes

---
+++ Library/Musico.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TossalERP.Library
+{
+    public enum Instrumento
+    {
+        Flauta, Saxo, Dulzaina, Clarinete, Oboe,Tuba, Trombon, Trompa,Percu
+    }
+    internal class Musico
+    {
+        public string Nombre;
+        public Instrumento Instrumento;
+        public int TotalGanado;
+    }
+}
